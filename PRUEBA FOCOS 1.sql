0c5efb0a979a4699b5670388e8eaa58b73036ee0
Penilla2002 <146791742+Penilla2002@users.noreply.github.com>
2025-10-06 11:41:02 -0600

    refuerzo del cmentario anterior
    se cargo correctamente

--- PRUEBA FOCOS 1.sql
+++ PRUEBA FOCOS 1.sql
@@ -1,2 +1,8 @@
-CREATE DATABASE sistema_focos;
-USE sistema_focos;
+-- Insertar categorías primero
+INSERT INTO categorias (nombre) VALUES 
+('LED'), ('Ahorrador'), ('Halógeno'), ('Incandescente');
+
+-- Luego productos
+INSERT INTO productos (nombre, categoria_id, costo_compra, precio_venta) VALUES
+('Foco LED 10W', 1, 8.50, 15.00),
+('Foco LED 15W', 1, 12.00, 22.00);categorias
