a3658ad65e1ebfe5462ccadc83044c52af97ab63
Penilla2002 <146791742+Penilla2002@users.noreply.github.com>
2025-10-06 11:43:38 -0600

    limpieza de consola
    se limpia la consola

--- PRUEBA FOCOS 1.sql
+++
@@ -1,8 +0,0 @@
--- Insertar categorías primero
-INSERT INTO categorias (nombre) VALUES 
-('LED'), ('Ahorrador'), ('Halógeno'), ('Incandescente');
-
--- Luego productos
-INSERT INTO productos (nombre, categoria_id, costo_compra, precio_venta) VALUES
-('Foco LED 10W', 1, 8.50, 15.00),
-('Foco LED 15W', 1, 12.00, 22.00);categorias
